182dec9b39982c8d1736ff74a7b4cd683216fec2
Petter Strandh <petter.strandh@gmail.com>
2021-02-04 00:24:03 +0100

    Moving all print to its own file

---
+++ print.hh
@@ -0,0 +1,28 @@
+#ifndef PRINT_HH
+#define PRINT_HH
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+
+#include "blf_structs.hh"
+
+
+std::string print(exit_codes ec);
+std::string print(ObjectType_e ot);
+std::string print(AppId_e ai);
+void print(std::ostream &s, const sysTime_t &ts);
+void print(std::ostream &s, const fileStatistics &os);
+void print(std::ostream &s, const ObjectHeaderBase &ohb);
+void print(std::ostream &s, const ObjectHeader2 &oh2);
+void print(std::ostream &s, const LogContainer &lc);
+void print(std::ostream &s, const ObjectHeader &oh);
+void print(std::ostream &s, const CanMessage &cm);
+void print(std::ostream &s, const CanOverload &co);
+void print(std::ostream &s, const CanMessage2 &cm2);
+void print(std::ostream &s, const AppTrigger &at);
+void print(std::ostream &s, const AppText &at);
+void print(std::ostream &s, const reserved_5 &r);
+
+#endif
