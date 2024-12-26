d0d2820a6f97c29fc0182773efac0b8726a59e63
Petter Strandh <petter.strandh@gmail.com>
2024-12-27 00:06:03 +0100

    Remade count by using blf_reader

--- count_id.cc
+++ count_id.cc
@@ -3,7 +3,6 @@
  * Contact: petter.strandh@gmail.com
  */
 
-// g++ count_id.cc -o count_id -Wall -Wextra -Wpedantic -Wno-switch -O0 -g
 
 #include <cstring>
 #include <fstream>
@@ -11,9 +10,42 @@
 #include <iostream>
 #include <vector>
 
+#include "blf_reader.hh"
 #include "blf_structs.hh"
-#include "count_id.hh"
 #include "print.hh"
+
+
+struct dbc_id_data
+{
+    uint8_t dbc_table;
+    uint32_t id;
+    std::string id_name;
+};
+
+
+struct dbc_names
+{
+    const char *const short_name;
+    const char *const file_name;
+};
+
+
+static const dbc_names DBC[] = {
+    {"Backbone1J1939_mod_DM1_v2.dbc", "Backbone1J1939_mod_DM1_v2.dbc"},
+    {"DiagnosticsLink.dbc", "DiagnosticsLink.dbc"},
+    {"DisplaySubnet.dbc", "DisplaySubnet.dbc"},
+    {"InfotainmentSubnet.dbc", "InfotainmentSubnet.dbc"},
+    {"bb1.dbc", "bb1.dbc"},
+    {"bb1_t2.dbc", "bb1_t2.dbc"},
+    {"bb2.dbc", "bb2.dbc"},
+    {"bb2_t2.dbc", "bb2_t2.dbc"},
+    {"engine_subnet.dbc", "engine_subnet.dbc"},
+    {"engine_subnet_t2.dbc", "engine_subnet_t2.dbc"},
+    {"ptcan.dbc", "ptcan.dbc"},
+    {"ptcan_t2.dbc", "ptcan_t2.dbc"},
+};
+
+const int DBC_number = 12;
 
 
 using namespace lblf;
@@ -37,9 +69,9 @@ struct can_counter_record
 // Global
 namespace GLOBAL
 {
-std::vector<can_counter_record> id_data; //NOLINT
-uint32_t can_frame_counter; //NOLINT
-}
+std::vector<can_counter_record> id_data; // NOLINT
+uint32_t can_frame_counter;              // NOLINT
+} // namespace GLOBAL
 
 
 auto can_id_name_func(std::string &can_id_name, const uint32_t can_id, const std::vector<dbc_id_data> &dbc_table) -> uint8_t
@@ -227,21 +259,13 @@ void print_dbcdata(std::ostream &stream, std::vector<dbc_id_data> &dbc_data)
 }
 
 
-auto fileLength(std::fstream &fs) -> uint32_t
-{
-    fs.seekg(0, std::fstream::end);
-    uint32_t length = fs.tellg();
-    fs.seekg(0, std::fstream::beg);
-    return length;
-}
-
 
 /**
  * @brief support operator for enum typed data.
  *
  */
 template <typename stream_type>
-std::istream &operator>>(stream_type &is, AppId_e &aid)
+auto operator>>(stream_type &is, AppId_e &aid) -> std::istream &
 {
     uint8_t temp;
     is >> temp;
@@ -255,7 +279,7 @@ std::istream &operator>>(stream_type &is, AppId_e &aid)
  *
  */
 template <typename stream_type>
-stream_type &operator>>(stream_type &is, sysTime_t &st)
+auto operator>>(stream_type &is, sysTime_t &st) -> stream_type &
 {
     is >> st.year;
     is >> st.month;
@@ -269,257 +293,64 @@ stream_type &operator>>(stream_type &is, sysTime_t &st)
 }
 
 
-/**
- * @brief read the beginning of the blf file.
- *
- */
-template <typename stream_type>
-auto read(stream_type &fs, fileStatistics &os) -> bool
-{
-    fs.read(reinterpret_cast<char *>(&os.FileSign), sizeof(os.FileSign));
-    if (os.FileSign != FileSignature)
-        {
-            return false;
-        }
-
-    fs.read(reinterpret_cast<char *>(&os.StatSize), sizeof(os.StatSize));
-    fs.read(reinterpret_cast<char *>(&os.AppId), sizeof(os.AppId));
-    fs.read(reinterpret_cast<char *>(&os.AppMaj), sizeof(os.AppMaj));
-    fs.read(reinterpret_cast<char *>(&os.AppMin), sizeof(os.AppMin));
-    fs.read(reinterpret_cast<char *>(&os.AppBuild), sizeof(os.AppBuild));
-    fs.read(reinterpret_cast<char *>(&os.ApiMaj), sizeof(os.ApiMaj));
-    fs.read(reinterpret_cast<char *>(&os.ApiMin), sizeof(os.ApiMin));
-    fs.read(reinterpret_cast<char *>(&os.ApiBuild), sizeof(os.ApiBuild));
-    fs.read(reinterpret_cast<char *>(&os.ApiPatch), sizeof(os.ApiPatch));
-    fs.read(reinterpret_cast<char *>(&os.fileSize), sizeof(os.fileSize));
-    fs.read(reinterpret_cast<char *>(&os.uncompressedSize), sizeof(os.uncompressedSize));
-    fs.read(reinterpret_cast<char *>(&os.objCount), sizeof(os.objCount));
-    fs.read(reinterpret_cast<char *>(&os.objRead), sizeof(os.objRead));
-    fs.read(reinterpret_cast<char *>(&os.meas_start_time), sizeof(os.meas_start_time));
-    fs.read(reinterpret_cast<char *>(&os.last_obj_time), sizeof(os.last_obj_time));
-    fs.read(reinterpret_cast<char *>(&os.fileSize_less115), sizeof(os.fileSize_less115));
-    auto offset = os.StatSize - sizeof(fileStatistics);
-    fs.seekg(offset, std::ios_base::cur);
-    return true;
-}
-
-
-auto read(std::fstream &fs, BaseHeader &ohb) -> bool
-{
-    fs.read(reinterpret_cast<char *>(&ohb.ObjSign), sizeof(ohb.ObjSign));
-    if (ohb.ObjSign != ObjectSignature)
-        {
-            std::cout << "Not Found LOBJ: " << std::hex << static_cast<int>(ohb.ObjSign);
-            std::cout << '\n';
-            return false;
-        }
-    fs.read(reinterpret_cast<char *>(&ohb.headerSize), sizeof(ohb.headerSize));
-    fs.read(reinterpret_cast<char *>(&ohb.headerVer), sizeof(ohb.headerVer));
-    fs.read(reinterpret_cast<char *>(&ohb.objSize), sizeof(ohb.objSize));
-    fs.read(reinterpret_cast<char *>(&ohb.objectType), sizeof(ohb.objectType));
-    return true;
-}
-
-
-auto read(const uint8_t *data, BaseHeader &ohb) -> bool
-{
-    std::memcpy(&ohb.ObjSign, data, sizeof(ohb.ObjSign));
-    if (ohb.ObjSign != ObjectSignature)
-        {
-            std::cout << "Not Found LOBJ: " << std::hex << static_cast<int>(ohb.ObjSign);
-            std::cout << '\n';
-            return false;
-        }
-    data = data + sizeof(ohb.ObjSign);
-    std::memcpy(&ohb.headerSize, data, sizeof(ohb.headerSize));
-    data = data + sizeof(ohb.headerSize);
-    std::memcpy(&ohb.headerVer, data, sizeof(ohb.headerVer));
-    data = data + sizeof(ohb.headerVer);
-    std::memcpy(&ohb.objSize, data, sizeof(ohb.objSize));
-    data = data + sizeof(ohb.objSize);
-    std::memcpy(&ohb.objectType, data, sizeof(ohb.objectType));
-    return true;
-}
-
-
-auto read(std::fstream &fs, LogContainer &lc, const BaseHeader &ohb) -> bool
-{
-    fs.read(reinterpret_cast<char *>(&lc), sizeof(LogContainer));
-
-    if (lc.compressionMethod == compressionMethod_e::uncompressed)
-        {
-            lc.unCompressedFileSize = ohb.objSize - sizeof(lc.compressionMethod) - sizeof(lc.reserv1) - sizeof(lc.reserv2) - sizeof(lc.unCompressedFileSize) - sizeof(lc.reserv3);
-        }
-
-    return true;
-}
-
-
 template <typename type_data>
-auto read_template(std::fstream &fs, type_data &data) -> bool
+auto read_template(const char *indata_array, type_data &data) -> size_t
 {
-    fs.read(reinterpret_cast<char *>(&data), sizeof(type_data));
-    return true;
+    std::memcpy(reinterpret_cast<char *>(&data), indata_array, sizeof(type_data));
+    return sizeof(type_data);
 }
 
 
-auto parse_container_uncompressed(std::fstream &fs, const LogContainer &lc) -> exit_codes
+
+
+
+auto main(int argc, char *argv[]) -> int
 {
-    int32_t bytes_left_in_container = lc.unCompressedFileSize;
-    bool run = true;
-    while (run)
+    if (argc != 2)
         {
-            struct BaseHeader ohb;
-            if (read(fs, ohb))
+            std::cout << "count_id <filename.blf>" << '\n';
+            return -1;
+        }
+
+    const std::string filename = argv[1];
+
+
+    std::string separator(" ");
+    std::vector<dbc_id_data> dbc_data;
+
+    for (int n = 0; n < DBC_number; n++)
+        {
+            std::string filename_dbc(DBC[n].file_name);
+            if (parse_dbc_file(filename_dbc, dbc_data, separator, n))
+                std::cout << "Success reading: " << DBC[n].file_name << '\n';
+        }
+
+    GLOBAL::can_frame_counter = 0;
+
+    lblf::blf_reader reader(filename);
+
+    while (reader.next())
+        {
+            const auto data = reader.data();
+            if (data.base_header.objectType == lblf::ObjectType_e::CAN_MESSAGE2)
                 {
-                    // print(std::cout, ohb);
-                }
-            else
-                return exit_codes::UNABLE_TO_READ_OBJECT_HEADER_BASE;
-
-            handle_ObjectType(fs, ohb);
-            bytes_left_in_container = bytes_left_in_container - ohb.objSize;
-            // std::cout << "LogContainer/ bytes left: " << std::dec << bytes_left_in_container << '\n';
-            if (bytes_left_in_container <= 0)
-                run = false;
-        }
-    return exit_codes::EXITING_SUCCESS;
-}
-
-
-auto handle_ObjectType(std::fstream &fs, const BaseHeader &ohb) -> exit_codes
-{
-    const auto payload_size = ohb.objSize - ohb.headerSize;
-    switch (ohb.objectType)
-        {
-        case (ObjectType_e::LOG_CONTAINER): // Get Logcontainer
-            {
-                struct LogContainer lc;
-                read(fs, lc, ohb);
-                if (lc.compressionMethod == compressionMethod_e::uncompressed)
-                    {
-                        // Lets work through the logcontainer.
-                        if (parse_container_uncompressed(fs, lc) == exit_codes::EXITING_SUCCESS)
-                            {
-                                std::cout << "LogContainer handled.\n";
-                            }
-                        else
-                            {
-                                std::cout << "LogContainer walk through failed.\n";
-                                return exit_codes::LOGCONTAINER_WALK_THROUGH_FAIL;
-                            }
-                    }
-            }
-            break;
-
-        case (ObjectType_e::CAN_MESSAGE): // read Can message;
-            {
-                struct ObjectHeader oh;
-                read_template(fs, oh);
-                if (payload_size == sizeof(CanMessage))
-                    {
-                        GLOBAL::can_frame_counter++;
-                        struct CanMessage cm;
-                        if (read_template(fs, cm))
-                            {
-                                unsigned int can_id = cm.id;
-                                unsigned char channel = cm.channel;
-                                can_id_count(GLOBAL::id_data, can_id, channel);
-                            }
-                    }
-                else
-                    {
-                        return exit_codes::CAN_MESSAGE_INVALID_LENGTH;
-                    }
-            }
-            break;
-
-        case (ObjectType_e::CAN_MESSAGE2):
-            {
-                GLOBAL::can_frame_counter++;
-                struct ObjectHeader oh;
-                read_template(fs, oh);
-                struct CanMessage2 cm2;
-                if (read_template(fs, cm2))
-                    {
-                        unsigned int can_id = cm2.id;
-                        unsigned char channel = cm2.channel;
-                        can_id_count(GLOBAL::id_data, can_id, channel);
-                    }
-                else
-                    {
-                        return exit_codes::CAN_MESSAGE_INVALID_LENGTH;
-                    }
-            }
-            break;
-
-        default:
-            {
-                auto bytes_to_jump = 0;
-                if (ohb.headerSize == 16)
-                    bytes_to_jump = ohb.objSize - ohb.headerSize;
-
-                if (ohb.headerSize == 32)
-                    bytes_to_jump = ohb.objSize - ohb.headerSize + 16;
-
-                fs.seekg(bytes_to_jump, std::ios_base::cur);
-            }
-        }
-    return exit_codes::EXITING_SUCCESS;
-}
-
-
-auto go_through_blf_file(const char *const filename) -> exit_codes
-{
-    std::cout << "Opening file: " << filename;
-    std::fstream fs(filename, std::fstream::in | std::fstream::binary);
-    if (fs.fail())
-        {
-            std::cout << " ";
-            std::cout << "File open failed, Exiting program\n";
-            return exit_codes::UNABLE_TO_OPEN_FILE;
-        }
-    else
-        {
-            std::cout << " ** SUCCESS **\n";
-        }
-
-    auto filelength = fileLength(fs);
-
-    if (fs)
-        {
-            struct fileStatistics fileStat;
-            if (read(fs, fileStat))
-                {
-                    print(std::cout, fileStat);
-                }
-            else
-                {
-                    std::cout << "Error file is not a BLF file\n";
-                    fs.close();
-                    return exit_codes::NOT_A_VALID_BLF_FILE;
+                    GLOBAL::can_frame_counter++;
+                    struct lblf::CanMessage2_obh can2;
+                    if (sizeof(can2) <= data.payload.size())
+                        {
+                            read_template(data.payload.data(), can2);
+                            unsigned int can_id = can2.id;
+                            unsigned char channel = can2.channel;
+                            can_id_count(GLOBAL::id_data, can_id, channel);
+                        }
                 }
         }
+    print_frameid_dbcdata(std::cout, GLOBAL::id_data, dbc_data);
+    std::cout << "Number of dbc frames: " << dbc_data.size() << '\n';
 
-    while (!fs.eof())
-        {
-            if ((filelength - fs.tellg() == 0))
-                break;
-            // std::cout << __FUNCTION__ << "Bytes left: " << filelength - fs.tellg() << '\n';
-            struct BaseHeader ohb;
-            if (read(fs, ohb))
-                {
-                    handle_ObjectType(fs, ohb);
-                }
-            else
-                {
-                    std::cout << __FUNCTION__ << ':' << __LINE__ << " Unable to read BaseHeader\n";
-                    fs.close();
-                    return exit_codes::UNABLE_TO_READ_OBJECT_HEADER_BASE;
-                }
-        }
-    fs.close();
-    return exit_codes::EXITING_SUCCESS;
+    std::cout << "Number of different frames: " << GLOBAL::id_data.size()
+              << " total number of frames in blf:" << GLOBAL::can_frame_counter << '\n';
+    return EXIT_SUCCESS;
 }
 
 
@@ -548,41 +379,3 @@ int main(int argc, char *argv[])
     return EXIT_SUCCESS;
 }
 #endif
-
-
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-        {
-            std::cout << "count_id <filename.blf>" << '\n';
-            return -1;
-        }
-
-    const char *const filename = argv[1];
-    std::string separator(" ");
-    std::vector<dbc_id_data> dbc_data;
-
-    for (int n = 0; n < DBC_number; n++)
-        {
-            std::string filename(DBC[n].file_name);
-            if (parse_dbc_file(filename, dbc_data, separator, n))
-                std::cout << "Success reading: " << DBC[n].file_name << '\n';
-        }
-
-    GLOBAL::can_frame_counter = 0;
-    exit_codes result = go_through_blf_file(filename);
-
-    if (result != exit_codes::EXITING_SUCCESS)
-        {
-            std::cout << print(result) << '\n';
-            std::cout << "Exiting\n";
-            return (EXIT_FAILURE);
-        }
-
-    print_frameid_dbcdata(std::cout, GLOBAL::id_data, dbc_data);
-    std::cout << "Number of dbc frames: " << dbc_data.size() << '\n';
-
-    std::cout << "Number of different frames: " << GLOBAL::id_data.size()
-              << " total number of frames in blf:" << GLOBAL::can_frame_counter << '\n';
-    return EXIT_SUCCESS;
-}
